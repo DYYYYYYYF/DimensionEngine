c36fd7ef80041ae9ed9cbd99a8d8ba94a1e928e8
DYYYYYYYF <64484752+DYYYYYYYF@users.noreply.github.com>
2023-07-16 19:25:54 +0800

    Delete RenderEngine/build directory

--- RenderEngine/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/uncled/Documents/CFiles/VulkanEngine/RendingEngine
-# Build directory: /Users/uncled/Documents/CFiles/VulkanEngine/RendingEngine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
