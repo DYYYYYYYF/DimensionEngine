42d323e731401e3ef116384ac87e0d512d5822a6
DYYYYYYYF <64484752+DYYYYYYYF@users.noreply.github.com>
2023-11-20 17:34:09 +0800

    Move resouces to render module

--- engine/Scene.cpp
+++ engine/Scene.cpp
@@ -14,52 +14,27 @@ Scene::~Scene() {
 void Scene::InitScene() {
 	_Renderer->Init();
 	
-	UploadMesh("../asset/model/room.obj", "room");
-	UploadMesh("../asset/model/ball.obj", "ball");
-	UploadMesh("../asset/model/CornellBox.obj", "CornellBox");
-
-	UploadMesh("../asset/model/sponza.obj", "sponza");
-	UploadMesh("../asset/model/bunny.obj", "Bunny");
-	UploadMesh("../asset/obj/wooden_boat/Boat.obj", "Boat");
-	
-	UploadTriangleMesh();
-	UploadRectangleMesh();
-
-	const char* defaultVertShader = "../shader/glsl/default_vert.spv";
-	const char* defaultFragShader = "../shader/glsl/default_frag.spv";
-	const char* meshFragShader = "../shader/glsl/texture_mesh_frag.spv";
-	const char* meshFloorVertShader = "../shader/glsl/mesh_floor_vert.spv";
-	const char* meshFloorFragShader = "../shader/glsl/mesh_floor_frag.spv";
-
-	Material deafaultMaterial;
-	_Renderer->CreatePipeline(deafaultMaterial, defaultVertShader, defaultFragShader);
-	_Materials["Default"] = deafaultMaterial;
-
-	Material deafaultMeshMaterial;
-	_Renderer->CreatePipeline(deafaultMeshMaterial, defaultVertShader, meshFragShader);
-	_Materials["Texture"] = deafaultMeshMaterial;
-
-	Material deafaultFloorMaterial;
-	_Renderer->CreatePipeline(deafaultFloorMaterial, meshFloorVertShader, meshFloorFragShader);
-	_Materials["Floor"] = deafaultFloorMaterial;
-
-	Material drawLineMaterial;
-	((Renderer*)_Renderer)->CreateDrawlinePipeline(drawLineMaterial);
-	_Materials["DrawLine"] = drawLineMaterial;
-
 	RenderObject floor;
-	floor.mesh = GetMesh("Rectangle");
-	floor.material = GetMaterial("Floor");
-	_Renderables.push_back(floor);
+	Mesh* rectMesh = ((Renderer*)_Renderer)->GetMesh("Rectangle");
+	Material* floorMat = ((Renderer*)_Renderer)->GetMaterial("Floor");
+	if (rectMesh != nullptr && floorMat != nullptr) {
+		floor.mesh = rectMesh;
+		floor.material = floorMat;
+		_Renderables.push_back(floor);
+	}
 
-	RenderObject triangle;
-	triangle.mesh = GetMesh("room");
-	triangle.material = GetMaterial("Texture");
-	triangle.SetTranslate(glm::vec3{ 0, 1, 0 });
-	triangle.SetRotate(glm::vec3{ 0, 0, 1 }, 90);
-	triangle.SetRotate(glm::vec3{ 0, 1, 0 }, 90);
-	triangle.SetRotate(glm::vec3{ 1, 0, 0 }, -90);
-	_Renderables.push_back(triangle);
+	RenderObject room;
+	Mesh* roomMesh = ((Renderer*)_Renderer)->GetMesh("room");
+	Material* textureMat = ((Renderer*)_Renderer)->GetMaterial("Texture");
+	if (roomMesh != nullptr && textureMat != nullptr) {
+		room.mesh = roomMesh;
+		room.material = textureMat;
+		room.SetTranslate(glm::vec3{ 0, 1, 0 });
+		room.SetRotate(glm::vec3{ 0, 0, 1 }, 90);
+		room.SetRotate(glm::vec3{ 0, 1, 0 }, 90);
+		room.SetRotate(glm::vec3{ 1, 0, 0 }, -90);
+		_Renderables.push_back(room);
+	}
 
 	INFO("Inited Scene.");
 }
@@ -140,123 +115,9 @@ void Scene::UpdatePosition(GLFWwindow* window) {
     ((Renderer*)_Renderer)->UpdateViewMat(_Camera.GetViewMatrix());
 }
 
-void Scene::UploadMesh(const char* filename, const char* mesh_name) {
-	Mesh tempMesh;
-	tempMesh.LoadFromObj(filename);
-	((Renderer*)_Renderer)->UploadMeshes(tempMesh);
-	_Meshes[mesh_name] = tempMesh;
-}
-
-void Scene::UploadTriangleMesh() {
-
-	Mesh triganleMesh;
-	//make the array 3 vertices long
-	triganleMesh.vertices.resize(3);
-
-	//vertex positions
-	triganleMesh.vertices[0].position = { 1.f, 2.f, 0.0f };
-	triganleMesh.vertices[1].position = { -1.f,2.f, 0.0f };
-	triganleMesh.vertices[2].position = { 0.f, 1.f, 0.0f };
-
-	//vertex colors
-	triganleMesh.vertices[0].color = { 1.f, 0.f, 0.0f };
-	triganleMesh.vertices[1].color = { 0.f, 1.f, 0.0f };
-	triganleMesh.vertices[2].color = { 0.f, 0.f, 1.0f };
-
-	// Normal
-	triganleMesh.vertices[0].normal = { 0.0f, 1.0f, 0.0f };
-	triganleMesh.vertices[1].normal = { 0.0f, 1.0f, 0.0f };
-	triganleMesh.vertices[2].normal = { 0.0f, 1.0f, 0.0f };
-
-	// UV
-	triganleMesh.vertices[0].texCoord = { 0.0f, 0.0f };
-	triganleMesh.vertices[1].texCoord = { 1.0f, 0.0f };
-	triganleMesh.vertices[2].texCoord = { 0.5f, 1.0f };
-
-	//indices
-	triganleMesh.indices.resize(3);
-	triganleMesh.indices[0] = 0;
-	triganleMesh.indices[1] = 1;
-	triganleMesh.indices[2] = 2;
-
-	((Renderer*)_Renderer)->UploadMeshes(triganleMesh);
-	_Meshes["Triangle"] = triganleMesh;
-
-	INFO("Loaded Triangle");
-}
-
-void Scene::UploadRectangleMesh() {
-	Mesh rectangleMesh;
-	//make the array 6 vertices long
-	rectangleMesh.vertices.resize(4);
-
-	//vertex positions
-	rectangleMesh.vertices[0].position = {  50.f, 0.f,  -50.f };	//右后
-	rectangleMesh.vertices[1].position = {  50.f, 0.f,   50.f };	//右前
-	rectangleMesh.vertices[2].position = { -50.f, 0.f,   50.f };	//左前
-	rectangleMesh.vertices[3].position = { -50.f, 0.f,  -50.f };	//左后
-
-	//vertex colors, all green
-	rectangleMesh.vertices[0].color = { 0.9f, 0.9f, 0.9f };
-	rectangleMesh.vertices[1].color = { 0.1f, 0.1f, 0.1f };
-	rectangleMesh.vertices[2].color = { 0.9f, 0.9f, 0.9f };
-	rectangleMesh.vertices[3].color = { 0.1f, 0.1f, 0.1f };
-
-	// Normal
-	rectangleMesh.vertices[0].normal = { 0.0f, 1.0f, 0.0f };
-	rectangleMesh.vertices[1].normal = { 0.0f, 1.0f, 0.0f };
-	rectangleMesh.vertices[2].normal = { 0.0f, 1.0f, 0.0f };
-	rectangleMesh.vertices[3].normal = { 0.0f, 1.0f, 0.0f };
-
-	// UV
-	rectangleMesh.vertices[0].texCoord = { 0.0f, 1.0f };
-	rectangleMesh.vertices[1].texCoord = { 1.0f, 1.0f };
-	rectangleMesh.vertices[2].texCoord = { 1.0f, 0.0f };
-	rectangleMesh.vertices[3].texCoord = { 0.0f, 0.0f };
-
-	//indices
-	rectangleMesh.indices.resize(6);
-	rectangleMesh.indices[0] = 0;
-	rectangleMesh.indices[1] = 1;
-	rectangleMesh.indices[2] = 2;
-	rectangleMesh.indices[3] = 0;
-	rectangleMesh.indices[4] = 2;
-	rectangleMesh.indices[5] = 3;
-
-	((Renderer*)_Renderer)->UploadMeshes(rectangleMesh);
-	_Meshes["Rectangle"] = rectangleMesh;
-	
-	INFO("Loaded Rectangle");
-}
-
-//returns nullptr if it can't be found
-Material* Scene::GetMaterial(const std::string& name) {
-	//search for the object, and return nullptr if not found
-	auto it = _Materials.find(name);
-	if (it == _Materials.end()) {
-		return nullptr;
-	}
-	else {
-		return &(*it).second;
-	}
-}
-
-//returns nullptr if it can't be found
-Mesh* Scene::GetMesh(const std::string& name) {
-	auto it = _Meshes.find(name);
-	if (it == _Meshes.end()) {
-		return nullptr;
-	}
-	else {
-		return &(*it).second;
-	}
-}
-
 void Scene::Destroy() {
 	if (_Renderer != nullptr)
 	{
-    ((Renderer*)_Renderer)->UnloadMeshes(_Meshes);
-    ((Renderer*)_Renderer)->DestroyMaterials(_Materials);
 		_Renderer->Release();
 		free(_Renderer);
 	}
@@ -265,7 +126,7 @@ void Scene::Destroy() {
 RenderObject Scene::GenerateBoudingBox(const Mesh* mesh) {
 	
 	RenderObject obj;
-	obj.material = GetMaterial("DrawLine");
+	obj.material = ((Renderer*)_Renderer)->GetMaterial("DrawLine");
 
 	float fx = 0.0f;
 	float fy = 0.0f;
@@ -327,9 +188,9 @@ RenderObject Scene::GenerateBoudingBox(const Mesh* mesh) {
 	boundingMesh.indices[14] = 6;
 	boundingMesh.indices[15] = 1;
 
-	((Renderer*)_Renderer)->UploadMeshes(boundingMesh);
-	_Meshes["Bouding"] = boundingMesh;
-	obj.mesh = GetMesh("Bouding");
+	((Renderer*)_Renderer)->LoadMesh("Bouding", boundingMesh);
+
+	obj.mesh = ((Renderer*)_Renderer)->GetMesh("Bouding");
 	obj.SetTranslate(glm::vec3{ 0.0f, 1.0f, 0.0f });
 
 	_Renderables.push_back(obj);
--- engine/Scene.hpp
+++ engine/Scene.hpp
@@ -22,26 +22,16 @@ public:
 	void InitScene();
 	void Update();
 	void Destroy();
-
-	void UploadMesh(const char* filename, const char* mesh_name);
-	void UploadTriangleMesh();
-	void UploadRectangleMesh();
 	void UpdatePosition(GLFWwindow* window);
-
-	Material* GetMaterial(const std::string& name);
-	Mesh* GetMesh(const std::string& name);
 
 	RenderObject GenerateBoudingBox(const Mesh* mesh);
 
 private:
 	IRenderer* _Renderer;
+	Camera _Camera = Camera(glm::vec3(0.0f, 10.0f, -10.0f), glm::radians(0.0f), glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 	//default array of renderable objects
 	std::vector<RenderObject> _Renderables;
-	std::unordered_map<std::string, Material> _Materials;
-	std::unordered_map<std::string, Mesh> _Meshes;
-
-	Camera _Camera = Camera(glm::vec3(0.0f, 10.0f, -10.0f), glm::radians(0.0f), glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 public:
 	uint32_t _FrameCount = 1;
