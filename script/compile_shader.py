fcc5a62e298d75b3b5f52b3e4f68c7b89edf199d
UncleDon <64484752+DYYYYYYYF@users.noreply.github.com>
2024-03-26 16:24:11 +0800

    Search dir shader files

--- script/compile_shader.py
+++ script/compile_shader.py
@@ -1,68 +1,91 @@
 import os
 import platform
 
-vulkanPath = "D:\C_Library\VulkanSDK"
-compile_command = "glslc -c "
-test_command = compile_command + "--version"
+# Common values
+vulkanPath       = "D:\C_Library\VulkanSDK"
+compile_command  = "glslc -c "
+file_path        = os.path.abspath(".") + "/shader/"
 
-filePath = os.path.abspath(".") + "/shader/"
+# compile failed files
+failed_shaders = []
 
-# Windows platform
-# Check glslc command
-if platform.system().lower() == "windows":
-    if os.system(test_command) != 0:
+def CheckGlslc():
+    # global value will be changed, so we need define the value inside func
+    global compile_command
 
-        test_command = filePath + "glslc.exe -c "
-        if os.system(test_command) != 0:
-
+    compile_check_command     = compile_command + "--version"
+    if os.system(compile_check_command) != 0:
+        compile_check_command = file_path + "glslc.exe -c "
+        
+        if os.system(compile_check_command) != 0:
             # Not set global command
             os.system("cls")
             compile_command = vulkanPath + "\Bin\glslc.exe -c "
-            test_command = compile_command + "--version "
+            compile_check_command = compile_command + "--version "
             
-            if os.system(test_command) != 0:
+            if os.system(compile_check_command) != 0:
                 # Not set right vulkan path
                 os.system("cls")
                 print("Please set vulkan path!")
                 quit()
 
-print("Shader direction: " + filePath + "\n")
-failedShaders = []
 
-for root, dirs, files in os.walk(filePath):
-    for file in files:
-        filename = os.path.splitext(file)
-        if filename[1] != ".spv" and filename[1] != ".exe":
-            if filename[1] == '': 
-                continue
+def Compile(sub_file_path, file, filename):
+    shader_type = filename[1].split('.')[1]
+    if filename[1] != ".hlsl":
+        ## Record compile command
+        cmd = compile_command + sub_file_path + "/" + file + " -o " + sub_file_path + "/" + filename[0] + "_" + shader_type + ".spv"
+        print("Compiling " + sub_file_path + "/" + filename[0] + filename[1])
 
-            shader_type = filename[1].split('.')[1]
-            if filename[1] != ".hlsl":
-                ## Record compile command
-                cmd = compile_command + filePath + "glsl/" + file + " -o " + filePath + "glsl/" + filename[0] + "_" + shader_type + ".spv"
-                print("Compiling " + filename[0] + filename[1])
+        ## Execute compile command
+        if os.system(cmd) != 0:
+            failed_shaders.append(file)
 
-                ## Execute compile command
-                if os.system(cmd) != 0:
-                    failedShaders.append(file)
+    else:
+        sub_filename = os.path.splitext(filename[0])
+        shader_type = sub_filename[1].split('.')[1]
 
-            else:
-                sub_filename = os.path.splitext(filename[0])
-                shader_type = sub_filename[1].split('.')[1]
+        ## Record compile command
+        cmd = compile_command + " -c -fshader-stage=" + shader_type + " -fentry-point=main " + sub_file_path \
+        + "/" + file + " -o " + sub_file_path + "/" + filename[0] + ".spv"
+        print("Compiling " + sub_file_path + "/" + filename[0] + filename[1])
 
-                ## Record compile command
-                cmd = compile_command + " -c -fshader-stage=" + shader_type + " -fentry-point=main " + filePath \
-                + "hlsl/" + file + " -o " + filePath + "hlsl/" + filename[0] + ".spv"
-                print("Compiling " + filename[0] + filename[1])
+        ## Execute compile command
+        if os.system(cmd) != 0:
+            failed_shaders.append(file)
 
-                ## Execute compile command
-                if os.system(cmd) != 0:
-                    failedShaders.append(file)
-                        
 
-if len(failedShaders) == 0:
-    print("\nAll shaders compiled successful...")
-else:
-    print("\nComile failed shaders:")
-    for failShader in failedShaders:
-        print(failShader)
+def SearchShaderFiles(sub_file_path):
+    for root, dirs, files in os.walk(sub_file_path):
+        for dir in dirs:
+            SearchShaderFiles(dir)
+        for file in files:
+            filename = os.path.splitext(file)
+            if filename[1] != ".spv" and filename[1] != ".exe" and filename[1] != '':
+                Compile(root, file, filename)
+
+def CompileShaders():
+    global failed_shaders
+    
+    # Windows platform
+    # Check glslc command
+    if platform.system().lower() == "windows":
+        CheckGlslc()
+
+    print("Shader direction: " + file_path + "\n")
+    SearchShaderFiles(file_path)
+
+    LogCompileResult()
+    
+
+def LogCompileResult():
+    if len(failed_shaders) == 0:
+        print("\nAll shaders compiled successful...")
+    else:
+        print("\nComile failed shaders:")
+        for fail_hader in failed_shaders:
+            print(fail_hader)
+
+
+if __name__ == '__main__':
+    CompileShaders()
