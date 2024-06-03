05ccea616d0af10b1c79a5c73f79ea687f9e33c8
UncleDonWindows <610175956@qq.com>
2024-06-03 19:28:41 +0800

    Ring queue structure.

---
+++ Engine/Containers/TQueue.hpp
@@ -0,0 +1,133 @@
+#pragma once
+
+#include "Defines.hpp"
+
+#include "Core/EngineLogger.hpp"
+#include "Core/DMemory.hpp"
+#include "Platform/Platform.hpp"
+
+/**
+ * @brief Represents a ring queue of a particular size. Does not resize dynamically.
+ * Naturally, this is a FIFO structure,
+ */
+template<typename ElementType>
+class RingQueue{
+public:
+	RingQueue() {
+		RingQueue(1024, nullptr);
+	}
+
+	/**
+	 * @brief Creates a new ring queue of the given capacity and stride.
+	 * 
+	 * @param capacity The total number of elements to be available in the queue.
+	 */
+	RingQueue(uint32_t capacity, ElementType* memory = nullptr) {
+		Length = 0;
+		Capacity = capacity;
+		Stride = sizeof(ElementType);
+		Head = 0;
+		Tail = -1;
+
+		if (memory) {
+			OwnsMemory = false;
+			Block = memory;
+		}
+		else {
+			OwnsMemory = true;
+			Block = (ElementType*)Memory::Allocate(Capacity * Stride, MemoryType::eMemory_Type_Ring_Queue);
+		}
+
+		ASSERT(Block);
+	}
+
+	/**
+	 * @brief Destroys the queue.
+	 */
+	void Clear() {
+		if (OwnsMemory) {
+			Memory::Free(Block, Capacity * Stride, MemoryType::eMemory_Type_Ring_Queue);
+			Block = nullptr;
+		}
+	}
+
+	/**
+	 * @brief Adds value to queue, if space is available.
+	 * 
+	 * @param value The value to be added.
+	 * @return True if success.
+	 */
+	bool Enqueue(ElementType* value) {
+		if (value == nullptr) {
+			return false;
+		}
+
+		if (Length == Capacity) {
+			UL_ERROR("Ring::Enqueue() Attempted to enqueue value in full ring queue: %p.", this);
+			return false;
+		}
+
+		Tail = (Tail + 1) % Capacity;
+		Memory::Copy(Block + (Tail * Stride), value, Stride);
+		Length++;
+		return true;
+	}
+
+	/**
+	 * @brief Attempts to retrieve the next value from the queue.
+	 * 
+	 * @param out_val A pointer to hold the retrieved value.
+	 * @return True if success;
+	 */
+	bool Dequeue(ElementType* out_val) {
+		if (out_val == nullptr) {
+			return false;
+		}
+
+		if (Length == 0) {
+			UL_ERROR("RingQueue::Dequeue() Attempted to dequeue value in empty ring queue.");
+			return false;
+		}
+
+		Memory::Copy(out_val, Block + (Head * Stride), Stride);
+		Head = (Head + 1) % Capacity;
+		Length--;
+		return true;
+	}
+
+	/**
+	 * @brief Attempts to retrieve, but not remove, the next value in the queue, if not empty.
+	 * 
+	 * @param out_val A pointer to hold the retrieved value.
+	 * @return True if success;
+	 */
+	bool Peek(ElementType* out_val) {
+		if (out_val == nullptr) {
+			return false;
+		}
+
+		if (Length == 0) {
+			UL_ERROR("RingQueue::Dequeue() Attempted to dequeue value in empty ring queue.");
+			return false;
+		}
+
+		Memory::Copy(out_val, Block + (Head * Stride), Stride);
+		return true;
+	}
+
+public:
+	uint32_t GetLength() const { return Length; }
+	uint32_t GetStride() const { return Stride; }
+	uint32_t GetCapacity() const { return Capacity; }
+	int GetHead() const { return Head; }
+	int GetTail() const { return Tail; }
+	
+private:
+	uint32_t Length;
+	uint32_t Stride;
+	uint32_t Capacity;
+	ElementType* Block;
+	bool OwnsMemory;
+	int Head;
+	int Tail;
+};
