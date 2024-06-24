fa3594244701cbd66234090b5226b8909805b130
UncleDonWindows <610175956@qq.com>
2024-06-24 20:19:18 +0800

    Optimize.

--- Shaders/hlsl/Builtin.WorldPickShader.vert.hlsl
+++ Shaders/hlsl/Builtin.WorldPickShader.vert.hlsl
@@ -14,23 +14,12 @@ struct PushConstant
 struct VSInput
 {
     [[vk::location(0)]] float3 vPosition : POSITION0;
-    [[vk::location(1)]] float3 vNormal : NORMAL0;
-    [[vk::location(2)]] float2 vTexCoord : TEXCOORD0;
-    [[vk::location(3)]] float4 vColor : COLOR0;
-    [[vk::location(4)]] float4 vTangent;
-};
-
-struct VSOutput
-{
-    float4 outPosition : SV_POSITION;
 };
 
 [[vk::binding(0, 0)]] UBO ubo;
 [[vk::push_constant]] ConstantBuffer<PushConstant> push_constants;
 
-VSOutput main(VSInput input)
+float4 main(VSInput input) : SV_POSITION
 {
-    VSOutput output = (VSOutput) 0;
-    output.outPosition = mul(ubo.proj, mul(ubo.view, mul(push_constants.model, float4(input.vPosition, 1.0f))));
-    return output;
+    return mul(ubo.proj, mul(ubo.view, mul(push_constants.model, float4(input.vPosition, 1.0f))));
 }
